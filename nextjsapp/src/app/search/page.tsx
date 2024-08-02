c4bb0d68b81fa316ddfa14a42fa42887a9f38d34
vai195 <vksonnakul@gmail.com>
2024-08-02 17:16:53 -0400

    fixed issues with search missing meeting time fields

--- nextjsapp/src/app/search/page.tsx
+++ nextjsapp/src/app/search/page.tsx
@@ -23,7 +23,9 @@ function Searchpage() {
     e.preventDefault();
     setLoading(true);
     const response = await fetch(
-      `${process.env.GCLOUD_URL}/api/classes?search=${search}`,
+      `${process.env.GCLOUD_URL}/api/classes?search=${encodeURIComponent(
+        search
+      )}`,
       { method: "GET" }
     );
     const json = await response.json();
